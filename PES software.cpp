412888011140bc02bd6528250c025b33daf699d5
492079 <66362282+492079@users.noreply.github.com>
2020-06-12 09:20:22 +0200

    Add files via upload

--- PES software.cpp
+++ PES software.cpp
@@ -19,8 +19,11 @@ DigitalIn BUTTON_LF(PA_10, PullUp);
 DigitalIn BUTTON_RF(PB_10, PullUp);
 DigitalIn BUTTON_BOTTOM(PA_8, PullUp);
 
-int state = -1; //initialiseer state
 enum states{FORWARD, TURN_RIGHT, TURN_LEFT, REVERSE, REVERSE_TURN}; //enum met alle states
+int state = FORWARD; //initialiseer state
+
+bool entry = 1;
+bool exiting = 0;
 
 int main(){
 
@@ -29,48 +32,164 @@ int main(){
         switch(state){ //state machine
 
             case FORWARD: //case voor naar voren rijden
-            LED_LEFT_FW = 1; //elke case heeft waarden voor alle leds
-            LED_RIGHT_FW = 1;
-            LED_LEFT_RV = 0;
-            LED_RIGHT_RV = 0;
-            wait(0.1); //wait 0.1 zodat de stofzuigerrobot iets langer doorrijdt
+            //entry
+            if(entry){
+                entry = 0;
+                LED_LEFT_FW = 1; //elke case heeft waarden voor alle leds
+                LED_RIGHT_FW = 1;
+                LED_LEFT_RV = 0;
+                LED_RIGHT_RV = 0;
+            }
+
+            //during
+            if(!BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als sensor links hoog
+                state = TURN_RIGHT;//naar rechts
+                exiting = 1;
+            }
+            else if(BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor rechts hoog
+                state = TURN_LEFT;//naar links
+                exiting = 1;
+            }
+            else if(!BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor links en rechts hoog
+                state = REVERSE;//naar achter
+                exiting = 1;
+            }
+            else if(BUTTON_LF && BUTTON_RF && !BUTTON_BOTTOM){ //als sensor onderkant hoog
+                state = REVERSE_TURN;//naar achter en draaien
+                exiting = 1;
+            }
+
+            //exit
+            if(exiting){
+                exiting = 0;
+                entry = 1;
+            }
+
+
             break; //break zodat de andere states worden overgeslagen
 
             case REVERSE: //case voor achteruit rijden
-            LED_LEFT_FW = 0;
-            LED_RIGHT_FW = 0;
-            LED_LEFT_RV = 1;
-            LED_RIGHT_RV = 1;
-            wait(0.1);   
+            //entry
+            if(entry){
+                entry = 0;
+
+                LED_LEFT_FW = 0;
+                LED_RIGHT_FW = 0;
+                LED_LEFT_RV = 1;
+                LED_RIGHT_RV = 1;
+            }
+
+            //during
+            if(BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als geen sensoren aan
+                state = FORWARD; //naar voren
+                exiting = 1;
+            }
+            else if(!BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als sensor links hoog
+                state = TURN_RIGHT;//naar rechts
+                exiting = 1;
+            }
+            else if(BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor rechts hoog
+                state = TURN_LEFT;//naar links
+                exiting = 1;
+            }
+
+            //exit
+            if(exiting){
+                exiting = 0;
+                entry = 1;
+            }
+
             break;  
 
             case TURN_RIGHT: //case voor naar rechts draaien
-            LED_LEFT_FW = 1;
-            LED_RIGHT_FW = 0;
-            LED_LEFT_RV = 0;
-            LED_RIGHT_RV = 1;      
-            wait(0.1);
+            //entry
+            if(entry){
+                entry = 0;
+
+                LED_LEFT_FW = 1;
+                LED_RIGHT_FW = 0;
+                LED_LEFT_RV = 0;
+                LED_RIGHT_RV = 1;  
+            }
+
+
+            //during
+            if(BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als geen sensoren aan
+                state = FORWARD; //naar voren
+                exiting = 1;
+            }
+            else if(BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor rechts hoog
+                state = TURN_LEFT;//naar links
+                exiting = 1;
+            }
+            else if(!BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor links en rechts hoog
+                state = REVERSE;//naar achter
+                exiting = 1;
+            }
+            else if(BUTTON_LF && BUTTON_RF && !BUTTON_BOTTOM){ //als sensor onderkant hoog
+                state = REVERSE_TURN;//naar achter en draaien
+                exiting = 1;
+            }
+
+            //exit
+            if(exiting){
+                exiting = 0;
+                entry = 1;
+            }
+
             break;   
 
             case TURN_LEFT: //case voor naar links draaien.
-            LED_LEFT_FW = 0;
-            LED_RIGHT_FW = 1;
-            LED_LEFT_RV = 1;
-            LED_RIGHT_RV = 0;
-            wait(0.1);
+            //entry
+            if(entry){
+                entry = 0;
+
+                LED_LEFT_FW = 0;
+                LED_RIGHT_FW = 1;
+                LED_LEFT_RV = 1;
+                LED_RIGHT_RV = 0;
+            }
+
+            //during
+            if(BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als geen sensoren aan
+                state = FORWARD; //naar voren
+                exiting = 1;
+            }
+            else if(!BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als sensor links hoog
+                state = TURN_RIGHT;//naar rechts
+                exiting = 1;
+            }
+            else if(!BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor links en rechts hoog
+                state = REVERSE;//naar achter
+                exiting = 1;
+            }
+            else if(BUTTON_LF && BUTTON_RF && !BUTTON_BOTTOM){ //als sensor onderkant hoog
+                state = REVERSE_TURN;//naar achter en draaien
+                exiting = 1;
+            }
+
+            //exit
+            if(exiting){
+                exiting = 0;
+                entry = 1;
+            }
             break;
 
             case REVERSE_TURN: //speciale case waarbij eerst achteruit gereden wrordt en daarna gedraaid
+            //entry&exit
             LED_LEFT_FW = 0;
             LED_RIGHT_FW = 0;
             LED_LEFT_RV = 1;
             LED_RIGHT_RV = 1;
             wait(1);
+
             LED_LEFT_FW = 1;
             LED_RIGHT_FW = 0;
             LED_LEFT_RV = 0;
             LED_RIGHT_RV = 1;
             wait(1);
+            
+            state = FORWARD;
             break;
 
             default: //default case, alles uit
@@ -79,22 +198,6 @@ int main(){
             LED_LEFT_RV = 0;
             LED_RIGHT_RV = 0;
             break;            
-        }
-
-        if(BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als geen sensoren aan
-            state = FORWARD; //naar voren
-        }
-        else if(!BUTTON_LF && BUTTON_RF && BUTTON_BOTTOM){ //als sensor links hoog
-            state = TURN_RIGHT;//naar rechts
-        }
-        else if(BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor rechts hoog
-            state = TURN_LEFT;//naar links
-        }
-        else if(!BUTTON_LF && !BUTTON_RF && BUTTON_BOTTOM){ //als sensor links en rechts hoog
-            state = REVERSE;//naar achter
-        }
-        else if(BUTTON_LF && BUTTON_RF && !BUTTON_BOTTOM){ //als sensor onderkant hoog
-            state = REVERSE_TURN;//naar achter en draaien
         }
 
     }
